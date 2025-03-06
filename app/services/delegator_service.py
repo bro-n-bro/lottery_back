d01472bd5c2df3d414f9121220ccb81240e9c6b8
iljagrabar14 <iljagrabar14@gmail.com>
2025-03-06 11:35:30 +0100

    Fix minor notes

--- app/services/delegator_service.py
+++ app/services/delegator_service.py
@@ -25,22 +25,25 @@ def get_stakers_ranking(db: Session):
     delegators_alias = aliased(Delegator)
     initial_delegators_alias = aliased(InitialDelegator)
 
-    ticket_expr = (delegators_alias.amount - initial_delegators_alias.amount) // 10
+    difference_expr = delegators_alias.amount - initial_delegators_alias.amount
+    ticket_expr = difference_expr // 10
 
     ranking_query = (
         db.query(
             delegators_alias.address,
-            func.coalesce(ticket_expr, 0).label("tickets")
+            func.coalesce(ticket_expr, 0).label("tickets"),
+            func.coalesce(difference_expr, 0).label("difference")
         )
         .join(
             initial_delegators_alias,
             delegators_alias.address == initial_delegators_alias.address
         )
         .filter(initial_delegators_alias.is_participate == True)
-        .filter(delegators_alias.amount - initial_delegators_alias.amount >= 0)
+        .filter(ticket_expr > 0)
         .order_by(func.coalesce(ticket_expr, 0).desc())
     ).all()
 
-    return [{"position": index + 1, "address": row.address, "tickets": row.tickets} for index, row in
-            enumerate(ranking_query)]
-
+    return [
+        {"position": index + 1, "address": row.address, "tickets": row.tickets, "stake_diff": row.difference}
+        for index, row in enumerate(ranking_query) if row.tickets > 0
+    ]
